26f5844bd3be4ae71bf5c455582c1ac523fbce1a
Alex Lavrenchuk <alexey@code-care.pro>
2020-03-11 15:56:21 +0200

    add setings to multer

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ const config = require("config");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const multer = require('multer');
-const upload = multer();
+// const upload = multer();
+const upload = multer({
+  dest: "photo",
+  limits: {
+    files: 5, // allow up to 5 files per request,
+    fieldSize: 2 * 1024 * 1024 // 2 MB (max file size)
+  },
+  fileFilter: (req, file, cb) => {
+    // allow images only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error('Only image are allowed.'), false);
+    }
+    cb(null, true);
+  }
+});
 
 const app = express();
 
